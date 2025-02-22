06b09780fe2818d845a025c98e25153efb4ca6a7
Jitendra Dabhi <dabhijitendra5927@gmail.com>
2025-02-22 21:22:47 +0530

    new project

---
+++ calc/script.js
@@ -0,0 +1,25 @@
+let buttons = document.querySelectorAll("button");
+let input = document.querySelector("input");
+
+let string = "";
+Array.from(buttons).forEach((button) =>{
+    button.addEventListener("click",(e)=>{
+        if(e.target.innerHTML ==  "="){
+            string = eval(string);
+            input.value = string;
+        }
+        else if(e.target.innerHTML === "AC"){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML === "DEL"){
+            string = string.slice(0,-1);
+            input.value = string
+        }
+        else{
+            string = string + e.target.innerHTML;
+            input.value = string; 
+
+        }
+    });
+});
